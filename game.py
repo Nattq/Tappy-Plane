171545daaebee7462bfbfeafea84303ecca31091
Nattq <nataliajelito3@gmail.com>
2021-05-20 14:40:26 +0200

    sprites lists added

--- game.py
+++ game.py
@@ -1,21 +1,48 @@
 import arcade
 
 SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 700
 SCREEN_TITLE = 'flappybird'
+
+CHARACTER_SCALING = 0.5
+PIPE_SCALING = 0.5
+COIN_SCALING = 0.5
 
 class MyGame(arcade.Window):
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        #lists of sprites
+        self.coin_list = None
+        self.pipe_list = None
+        self.player_list = None
+
+        self.player_sprite = None
+
         arcade.set_background_color(arcade.csscolor.BEIGE)
-
     def setup(self):
-        pass
+        #create the sprite lists
+        self.player_list = arcade.SpriteList()
+        self.pipe_list = arcade.SpriteList(use_spatial_hash=True)
+        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
 
+        image_bird ='images/ghost.jpg'
+        self.player_sprite = arcade.Sprite(image_bird,CHARACTER_SCALING)
+        self.player_sprite.center_x = 100
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
+
+        pipe = arcade.Sprite('images/pipe.png', PIPE_SCALING)
+        pipe.position = [400, 400]
+        self.pipe_list.append(pipe)
+        
     def on_draw(self):
         '''render the screen'''
         arcade.start_render()
+
+        self.player_list.draw()
+        self.pipe_list.draw()
+        self.coin_list.draw()
 
 def main():
     window = MyGame()
