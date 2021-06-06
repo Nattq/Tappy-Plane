0b6b8ad1fc905c481e2d238d7ef9f8814ea9d1f4
Nattq <nataliajelito3@gmail.com>
2021-06-06 11:08:29 +0200

    nowe rury

--- pipe.py
+++ pipe.py
@@ -3,7 +3,7 @@ from constants import *
 import random 
 class Pipe(arcade.Sprite):
     def __init__(self, image,scale=.5):
-        super().__init__(image, scale)
+        super().__init__(image, scale,hit_box_algorithm='Detailed')
         self.speed = 2.5
         self.scored = False
 
@@ -12,12 +12,18 @@ class Pipe(arcade.Sprite):
 
     @classmethod
     def generate_pipe(cls):
-        pipe = 'images/bamboo1.png'
-        p1 = cls(pipe, PIPE_SCALING)
-        p1.bottom = random.randint(-100,200)
-        p1.left  = SCREEN_WIDTH
-
-        p2 = cls(pipe, PIPE_SCALING)
-        p2.bottom = p1.bottom + PIPE_GAP
-        p2.left  = SCREEN_WIDTH       
+        pipe_bottom = 'images/rock.png'
+        scale=random.uniform(0.7*PIPE_SCALING,1.3*PIPE_SCALING)
+        p1 = cls(pipe_bottom, scale)
+        p1.add_spatial_hashes()
+        p1.bottom = 0
+        p1.left  = SCREEN_WIDTH+50
+        #p1.draw_hit_box(line_thickness=4)
+        
+        pipe_top = 'images/rockDown.png'
+        p2 = cls(pipe_top, PIPE_SCALING*2-scale)
+        p2.bottom = p1.top + PIPE_GAP
+        p2.center_x = p1.center_x
+        p2.height = SCREEN_HEIGHT-p1.height-PIPE_GAP
+        p2.add_spatial_hashes()
         return p1,p2
